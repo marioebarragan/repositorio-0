71cdd79e74bbe64d34d878b1161eca1925e9e8af
mebarragan <marioebarragan@gmail.com>
2018-05-17 08:58:13 -0300

    AGREGO ENLACE EXTERNO

--- vamosAcorrer/js/js1.js
+++ vamosAcorrer/js/js1.js
@@ -1,13 +1,9 @@
-var alt= medioArbol(5);
-
-document.getElementById("demo").innerHTML = alt;
-
 function medioArbol(altura)
 {
-	var arbol="<br>";
-for (var i = 1; i<= altura; i++) { 
-    arbol += '*'.repeat(i) + "<br>";
-	
-} 	
-return arbol;
+  var arbol="<br>";
+  for (var i = 1; i<= altura; i++) { 
+  arbol += '*'.repeat(i) + "<br>";
+   } 	
+  return arbol;
 }
+   
