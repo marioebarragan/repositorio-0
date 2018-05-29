e218961a9cb5a633d7ba84a8230b6300ec981722
BAS-BBLA-N1-D05\Punto Digital <marioebarragan@gmail.com>
2018-05-29 20:27:09 -0300

    Se agregan contacto y los script para las cookie.

--- vamosAcorrer/js/cambiarTema.js
+++ vamosAcorrer/js/cambiarTema.js
@@ -1,5 +1,14 @@
-function DarkLight() {
+$(function(){
+	var claseActual = getCookie("currentClass");
+	var clasePrevia = claseActual == "dark-mode" ? "light-mode" : "dark-mode";
+	//$("#body").removeClass(clasePrevia);
+	//$("#body").addClass(claseActual);
+	$("#body").toggleClass(claseActual + " " + clasePrevia);
+});
+
+function setTheme() {
   var body = document.getElementById("body");
   var currentClass = body.className;
   body.className = currentClass == "dark-mode" ? "light-mode" : "dark-mode";
+  setCookie("currentClass", body.className, 365);  
 }
